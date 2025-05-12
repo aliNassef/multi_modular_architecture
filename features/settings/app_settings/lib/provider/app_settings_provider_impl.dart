6295665792a86755c7aaf86fe703774773ff2caf
aliNassef <anassef798@gmail.com>
2025-05-12 20:40:20 +0300

    feat: implement AppSettingsProvider with environment-specific implementations

---
+++ features/settings/app_settings/lib/provider/app_settings_provider_impl.dart
@@ -0,0 +1,28 @@
+import 'package:app_settings/provider/app_settings_provider.dart';
+import 'package:injectable/injectable.dart';
+
+@Injectable(as: AppSettingsProvider, env: [Environment.prod])
+class ProdAppSettingsProviderImpl implements AppSettingsProvider {
+  @override
+  String getAppLanguage() {
+    return 'en';
+  }
+
+  @override
+  String getAppTheme() {
+    return 'light';
+  }
+}
+
+@Injectable(as: AppSettingsProvider, env: [Environment.dev])
+class DevAppSettingsProviderImpl implements AppSettingsProvider {
+  @override
+  String getAppLanguage() {
+    return 'en';
+  }
+
+  @override
+  String getAppTheme() {
+    return 'dark';
+  }
+}
